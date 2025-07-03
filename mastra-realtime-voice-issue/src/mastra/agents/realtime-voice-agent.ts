7f00164fd1faf02ef88c246a08ca13e7af5fe890
wok <wok@example.com>
2025-07-04 04:03:11 +0900

    OpenAIRealtimeVoice cannot handle error.

--- mastra-realtime-voice-issue/src/mastra/agents/realtime-voice-agent.ts
+++ mastra-realtime-voice-issue/src/mastra/agents/realtime-voice-agent.ts
@@ -1,7 +1,6 @@
 import { Agent } from "@mastra/core/agent";
 import { OpenAIRealtimeVoice } from "@mastra/voice-openai-realtime";
 import { openai } from "@ai-sdk/openai";
-import { readWavAsInt16Array } from "./readwav";
 
 const voice = new OpenAIRealtimeVoice({
   model: "gpt-4o-mini-realtime-preview",
@@ -24,9 +23,6 @@ export const realtimeVoiceAgent = new Agent({
   voice,
 });
 
-// realtimeVoiceAgent.voice.on("speaking", ({ audio }) => {
-//   console.log("speaking", (audio as Buffer).length);
-// });
 
 realtimeVoiceAgent.voice.on("writing", ({ text, role }) => {
   console.log(`${role} said(w): ${text}`);
@@ -36,18 +32,15 @@ realtimeVoiceAgent.voice.on("error", (error) => {
   console.error("Voice error:", error);
 });
 
-// Listen for agent audio responses
-// realtimeVoiceAgent.voice.on("speaker", (data) => {
-//   console.log("speaker:", data);
-// });
 console.log("realtime voice connect...");
 await realtimeVoiceAgent.voice.connect();
 
 console.log("realtime voice start");
 realtimeVoiceAgent.voice.speak("Hello, I'm your AI assistant!");
 
-console.log("realtime voice send...");
-const int16 = readWavAsInt16Array("../../test.wav");
-realtimeVoiceAgent.voice.send(int16.data);
+console.log("realtime voice start");
+realtimeVoiceAgent.voice.speak("Hello, I'm your AI assistant!");
 
 console.log("realtime voice fin....");
+
+
